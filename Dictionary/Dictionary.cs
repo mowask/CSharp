f6fae35724631385c250a5eaa7cdfc26d3e8a0f2
mowask <164919999+mowask@users.noreply.github.com>
2024-08-26 17:44:39 +0300

    Add files via upload

---
+++ Dictionary/Dictionary.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Dictionary
+{
+    internal class Dictionary
+    {
+        string _name {  get; set; }
+        string _dictionaryFilePath { get; set; }
+
+        Dictionary (string name)
+        {
+            this._name = name;            
+        }
+
+        public virtual void LoadDictionary()
+        {
+           
+        }
+    }
+}
---
+++ Dictionary/EnglishRussian.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class EnglishRussian : Dictionary
+    {
+
+        string dictionaryFilePath = "dictionary.txt";
+
+        EnglishRussian(string name, string path) : Dictionary base(, )
+
+
+        public override void LoadDictionary(dictionaryFilePath)
+        {
+            if (File.Exists(dictionaryFilePath))
+            {
+                string[] lines = File.ReadAllLines(dictionaryFilePath);
+                foreach (var line in lines)
+                {
+                    var parts = line.Split(':');
+                    if (parts.Length == 2)
+                    {
+                        var word = parts[0];
+                        var translations = parts[1].Split(',');
+                        dictionary[word] = new List<string>(translations);
+                    }
+                }
+            }
+
+        }
+    }
+}
