bfdc71ab24644b865e3936798086efa0691a5735
mowask <164919999+mowask@users.noreply.github.com>
2024-09-04 17:20:49 +0300

    Add files via upload

---
+++ C#_HomeWork/CS_HW_modul_06/m6_2/Kettle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_HW_m06
+{
+    internal class Kettle : Device
+    {
+        public Kettle() { }
+        public Kettle(string name) : base(name) { }
+        public Kettle(string name, string description) : base(name, description) { }
+
+        public override void Sound()
+        {
+            Console.WriteLine("Kettle sound: whish");
+        }
+        public override void Show()
+        {
+            base.Show();
+        }
+        public override void Description()
+        {
+            base.Description();
+        }
+       
+
+    }
+}
---
+++ C#_HomeWork/CS_HW_modul_06/m6_2/Microwave.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_HW_m06
+{
+    internal class Microwave : Device
+    {
+        public Microwave() { }
+        public Microwave(string name) : base(name) { }
+        public Microwave(string name, string description) : base(name, description) { }
+
+        public override void Sound()
+        {
+            Console.WriteLine("Microwave sound: hum");
+        }
+        public override void Show()
+        {
+            base.Show();
+        }
+        public override void Description()
+        {
+            base.Description();
+        }
+       
+    }
+}
