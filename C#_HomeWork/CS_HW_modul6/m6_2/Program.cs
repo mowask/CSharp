30413c7bdda70d26fb50f609c5961b05d60f9a37
mowask <164919999+mowask@users.noreply.github.com>
2024-09-04 17:19:54 +0300

    Add files via upload

---
+++ C#_HomeWork/CS_HW_modul6/m6_2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_HW_m06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Car car = new Car("Ferrari", "sportcar");
+            car.Sound();
+            car.Show();
+            car.Description();                        
+            Console.WriteLine();
+
+            Kettle kettle = new Kettle("", "Not electric");
+            kettle.Sound();            
+            kettle.Description();
+            Console.WriteLine();
+
+            Microwave microwave = new Microwave("LG");
+            microwave.Sound();
+            microwave.Show();
+            Console.WriteLine();
+
+            SteamShip steamship = new SteamShip();
+            steamship.Sound();
+            
+            
+
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}
