545cb71f1b1c279d6316372e02ec4418c6c11b0d
mowask <164919999+mowask@users.noreply.github.com>
2024-09-04 12:41:57 +0300

    Add files via upload

---
+++ C#_HomeWork/Wpf_Hw_1/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_hw_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random random = new Random();
+        List<int> numbers = new List<int>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            button.Click += ButtonClick;
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (numbers.Count == 10)
+            {
+                MessageBox.Show("Ты врун! ", "Угадайка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                Close();
+                return;
+            }
+
+            int guessNumber;
+            do
+            {
+                guessNumber = random.Next(1, 11);
+            }
+            while (numbers.Contains(guessNumber));
+            {
+                numbers.Add(guessNumber);
+                MessageBoxResult result = MessageBox.Show($"Ваше число {guessNumber}?", "Угадайка",
+                                                MessageBoxButton.YesNo, MessageBoxImage.Question);
+                    if (result == MessageBoxResult.Yes)                   
+                    {
+                        myTextBlock.Text = $"Угадал с {numbers.Count} попытки! Ваше число {guessNumber}";
+                        numbers.Clear();
+                    }                
+            }
+        }
+    }
+}
+    
+
