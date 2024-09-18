b5bb7fe627f5c81b4809d356ebe47fc99f03e4fe
mowask <164919999+mowask@users.noreply.github.com>
2024-09-18 21:19:19 +0300

    Add files via upload

---
+++ WPF/WPF_L9 Menu Files/9_1_ListView/MainWindow.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+
+namespace Wpf_L9_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const string FILE_NAME = "people.xml";
+
+        ObservableCollection<Person> peopleList;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadPeapleFromFile();
+
+            //peopleList = new ObservableCollection<Person>()
+            //{ new Person ()
+            //    {
+            //    Name = "John",
+            //    Age = 19,
+            //    Email = "john@mail.ru"
+            //    },
+            //    new Person()
+            //    {
+            //        Name = "Jane",
+            //        Age = 18,
+            //        Email = "jane@mail.ru"
+            //    },
+            //    new Person()
+            //    {
+            //        Name = "Tom",
+            //        Age = 29,
+            //        Email = "tom@mail.ru"
+            //    }
+            //};
+
+            peopleListView.ItemsSource = peopleList;
+
+            btnAdd.Click += BtnAdd_Click;
+
+        }
+
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string name = textBoxName.Text;
+                if (name == string.Empty)
+                    throw new Exception("Name is empty");
+                int age = int.Parse(textBoxAge.Text);
+                string email = textBoxEmail.Text;
+                if (email == string.Empty)
+                    throw new Exception("Email is empty");
+
+                peopleList.Add(new Person()
+                {
+                    Name = name,
+                    Age = age,
+                    Email = email
+                });
+
+                SavePeapleToFile();
+
+                textBoxName.Text = string.Empty;
+                textBoxAge.Text = string.Empty;
+                textBoxEmail.Text = string.Empty;
+
+                //peopleListView.ItemsSource = peopleList;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+
+        private void SavePeapleToFile()
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(FILE_NAME, FileMode.Create))
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(ObservableCollection<Person>));
+
+                    serializer.Serialize(fs, peopleList);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+
+        private void LoadPeapleFromFile()
+        {
+            if (!File.Exists(FILE_NAME)) return;
+            try
+            {
+                using (FileStream fs = new FileStream(FILE_NAME, FileMode.Open))
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(ObservableCollection<Person>));
+
+                    peopleList= (ObservableCollection<Person>)serializer.Deserialize(fs);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+    }
+}
---
+++ WPF/WPF_L9 Menu Files/9_2_Menu_Files/MainWindow.xaml.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_L9_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private string _currentFilePath;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            menuItemExit.Click += MenuItemExit_Click;
+
+            menuItemNew.Click += MenuItemNew_Click;
+            menuItemOpen.Click += MenuItemOpen_Click;
+            menuItemSave.Click += MenuItemSave_Click;
+            menuItemSaveAs.Click += MenuItemSaveAs_Click;
+
+            textBox.TextChanged += TextBox_TextChanged;
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            textBlockLines.Text = "Lines " + (textBox.Text.Count(x => x == '\n') + 1);
+            textBlockChars.Text = "Chars " + textBox.Text.Count(x => x != '\n' && x != '\r');
+        }
+
+        private void MenuItemSaveAs_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                _currentFilePath = saveFileDialog.FileName;
+
+                using (StreamWriter writer = new StreamWriter(_currentFilePath))
+                {
+                    writer.Write(textBox.Text);
+                }
+                Title = _currentFilePath;
+            }
+        }
+
+        private void MenuItemSave_Click(object sender, RoutedEventArgs e)
+        {          
+            if (_currentFilePath != null)
+            {
+                using (StreamWriter writer = new StreamWriter(_currentFilePath))
+                {
+                    writer.Write(textBox.Text);
+                }
+            }
+            else
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    _currentFilePath = saveFileDialog.FileName;
+
+                    using (StreamWriter writer = new StreamWriter(_currentFilePath))
+                    {
+                        writer.Write(textBox.Text);
+                    }
+                    Title = _currentFilePath;
+                }
+            }
+
+          
+        }
+
+        private void MenuItemOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                _currentFilePath= openFileDialog.FileName;
+
+                try
+                {
+                    using (StreamReader reader = new StreamReader(_currentFilePath))
+                    {
+                        textBox.Text = reader.ReadToEnd();
+                    }
+                }
+                catch (Exception ex) 
+                {
+                    MessageBox.Show("Error open file:" + ex.Message);
+                }
+                Title = _currentFilePath;
+            }
+
+
+        }
+
+        private void MenuItemNew_Click(object sender, RoutedEventArgs e)
+        {
+            //
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            //
+            saveFileDialog.Filter = "text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+
+            //
+            if (saveFileDialog.ShowDialog() ==true)
+            {
+                //
+                _currentFilePath = saveFileDialog.FileName;
+
+                //
+                textBox.Text = string.Empty;
+
+                //
+                Title = _currentFilePath;
+
+            }
+
+        }
+
+        private void MenuItemExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
