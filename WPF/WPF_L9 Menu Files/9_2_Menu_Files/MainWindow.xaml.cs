9aefd957107c69bde6e9191a960b5e24961f2537
mowask <164919999+mowask@users.noreply.github.com>
2024-09-20 19:41:29 +0300

    Add files via upload

--- WPF/WPF_L9 Menu Files/9_2_Menu_Files/MainWindow.xaml.cs
+++ WPF/WPF_L9 Menu Files/9_2_Menu_Files/MainWindow.xaml.cs
@@ -37,8 +37,62 @@ namespace Wpf_L9_2
 
             textBox.TextChanged += TextBox_TextChanged;
 
+            btnCopy.Click += BtnCopy_Click;
+            btnCut.Click += BtnCut_Click;
+            btnPaste.Click += BtnPaste_Click;
+
+            btnLeft.Click += BtnLeft_Click;
+            btnRigt.Click += BtnRigt_Click;
+            btnCenter.Click += BtnCenter_Click;
+
+            comboBox.SelectionChanged += ComboBox_SelectionChanged;
+
         }
 
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (comboBox.SelectedIndex == 0) 
+                textBox.Foreground = new SolidColorBrush(Colors.Red);
+            if (comboBox.SelectedIndex == 1) 
+                textBox.Foreground = new SolidColorBrush(Colors.Green);
+            if (comboBox.SelectedIndex == 2) 
+                textBox.Foreground = new SolidColorBrush(Colors.Blue);
+
+        }
+
+        private void BtnCenter_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.HorizontalContentAlignment = HorizontalAlignment.Center;
+        }
+        private void BtnRigt_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.HorizontalContentAlignment = HorizontalAlignment.Right;
+        }
+        private void BtnLeft_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.HorizontalContentAlignment = HorizontalAlignment.Left;
+        }
+
+        private void BtnPaste_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.SelectedText += Clipboard.GetText();
+        }
+        private void BtnCut_Click(object sender, RoutedEventArgs e)
+        {
+            CopySelectedText();
+            textBox.SelectedText = string.Empty;
+        }
+        private void BtnCopy_Click(object sender, RoutedEventArgs e)
+        {
+            CopySelectedText();
+        }
+        private void CopySelectedText ()
+        {
+            if (textBox.SelectedText == string.Empty) return;
+            Clipboard.SetDataObject(textBox.SelectedText);
+        }
+
+        
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             textBlockLines.Text = "Lines " + (textBox.Text.Count(x => x == '\n') + 1);
