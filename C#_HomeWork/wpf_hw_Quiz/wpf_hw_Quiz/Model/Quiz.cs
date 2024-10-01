685c27ef3f6bc9e36cd9b85b1e81b750cd6cbc22
mowask <164919999+mowask@users.noreply.github.com>
2024-10-01 21:47:46 +0300

    Add files via upload

---
+++ C#_HomeWork/wpf_hw_Quiz/wpf_hw_Quiz/Model/Quiz.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wpf_hw_Quiz.Model;
+
+
+namespace wpf_hw_Quiz.Model
+{
+    public class Quiz
+    {
+        public List<Question> Questions { get; set; }
+        public string Name { get; set; }
+        public Quiz(List<Question> questions, string name)
+        {
+            Questions = questions;
+            Name = name;
+        }
+        public static Quiz GenerateQuiz()
+        {
+            List<Question> questions = new List<Question>();
+            for (int j = 0; j < 10; j++)
+            {
+                List<Answer> answers = new List<Answer>();
+                for (int i = 0; i < 3; i++)
+                {
+                    answers.Add(new Answer(i + 1,
+                                           $"Option {i + 1}",
+                                           i == 1));
+                }
+                questions.Add(new Question(j + 1,
+                                           $"Question {j + 1}?",
+                                           answers));
+            }
+            return new Quiz(questions, "Quiz name");
+        }
+    }
+}
---
+++ C#_HomeWork/wpf_hw_Quiz/wpf_hw_Quiz/ViewModel/QuizViewModel.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+using wpf_hw_Quiz.Model;
+
+namespace wpf_hw_Quiz.ViewModel
+{
+    public class QuizViewModel : INotifyPropertyChanged
+    {
+        public Quiz CurrentQuiz { get; set; }
+        public Answer CurrentAnswer { get; set; }
+
+        private Question _currentQuestion;
+        public Question CurrentQuestion
+        {
+            get { return _currentQuestion; }
+            set
+            {
+                _currentQuestion = value;
+                OnPropertyChanged("CurrentQuestion");
+            }
+        }
+        private int _countCurrent;
+        public int CountCurrent
+        {
+            get { return _countCurrent; }
+            set
+            {
+                _countCurrent = value;
+                OnPropertyChanged("CountCurrent");
+            }
+        }
+        private int _countCorrect;
+        public int CountCorrect
+        {
+            get { return _countCorrect; }
+            set
+            {
+                _countCorrect = value;
+                OnPropertyChanged("CountCorrect");
+            }
+        }
+
+        public ICommand SelectedAnswer { get; set; }
+        public ICommand NextQuestion { get; set; }
+
+        public QuizViewModel()
+        {
+            CurrentQuiz = Quiz.GenerateQuiz();
+            CurrentQuestion = CurrentQuiz.Questions[0];
+            CountCurrent = 1;
+            CountCorrect = 0;
+
+            SelectedAnswer = new DelegateCommand(SelectedAnswerHandler);
+            NextQuestion = new DelegateCommand(NextQuestionHandler);
+        }
+        public void SelectedAnswerHandler(Object obj)
+        {
+            if (obj is Answer)
+            {
+                CurrentAnswer = obj as Answer;
+
+                foreach (Answer answer in CurrentQuestion.Answers)
+                {
+                    answer.SetColor(false);
+                }
+
+                CurrentAnswer.SetColor(true);
+
+                Question temp = CurrentQuestion;
+                CurrentQuestion = null;
+                CurrentQuestion = temp;
+            }
+        }
+        public void NextQuestionHandler(Object obj)
+        {
+            if (CountCurrent >= 10)
+            {
+                MessageBox.Show($"Your result is: {CountCorrect}", "Congrats!", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+
+            if (CurrentAnswer == null)
+            {
+                MessageBox.Show("CurrentAnswer == null", "ERROR", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (CurrentAnswer.IsCorrect)
+                CountCorrect++;
+
+            CountCurrent++;
+            CurrentQuestion = CurrentQuiz.Questions[CountCurrent - 1];
+
+            CurrentAnswer = null;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
