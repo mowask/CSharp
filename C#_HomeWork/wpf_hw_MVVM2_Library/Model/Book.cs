9dcbf15dec30b116064e7c702f6d1f86cc09ab1a
mowask <164919999+mowask@users.noreply.github.com>
2024-10-13 19:54:49 +0300

    Add files via upload

---
+++ C#_HomeWork/wpf_hw_MVVM2_Library/Model/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_hw_MVVM2_Library.Model
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title} by {Author}, {Year}, {Genre}, {Description}";
+        }
+    }
+}
