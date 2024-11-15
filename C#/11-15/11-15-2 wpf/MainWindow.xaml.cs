9947a2389d66b3089e5b82530d6ed8e97e37ba03
mowask <164919999+mowask@users.noreply.github.com>
2024-11-15 21:24:55 +0300

    Add files via upload

---
+++ C#/11-15/11-15-2 wpf/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Data;
+
+namespace _11_15_wpf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public DataSet DataSet { get; set; }   
+        public SqlDataAdapter SqlDataAdapter { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataSet = new DataSet();
+
+            SqlConnection connection = null;
+            string connectionString = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
+
+            try
+            {                
+                connection = new SqlConnection(connectionString);
+                SqlDataAdapter = GetAuthorsAdapter(connection);
+                UpdateGridDataSource();
+               
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+
+
+        }
+
+        public void UpdateGridDataSource()
+        {
+            //  очищаем ДатаСет перед повторным заполнением
+            DataSet.Clear();
+
+            //  заполняем ДатаСет новыми данными из таблицы Афторс
+            SqlDataAdapter.Fill(DataSet, "Authors");
+
+            //  Устанавливаем источние данных для грида
+            grid.ItemsSource = DataSet.Tables["Authors"].DefaultView;
+        }
+
+        public static SqlDataAdapter GetAuthorsAdapter(SqlConnection connection)
+        {
+            string selectCommand = @"SELECT * FROM Authors";
+
+            SqlDataAdapter authorsAdapter = new SqlDataAdapter(selectCommand, connection);
+            SqlCommandBuilder builder = new SqlCommandBuilder(authorsAdapter);
+
+            return authorsAdapter;
+        }
+
+
+    }
+}
