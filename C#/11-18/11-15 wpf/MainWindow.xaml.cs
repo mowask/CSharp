a14092161f908b383d259588f6c8b59dfb575678
mowask <164919999+mowask@users.noreply.github.com>
2024-11-18 21:30:43 +0300

    Add files via upload

---
+++ C#/11-18/11-15 wpf/MainWindow.xaml.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Data;
+
+namespace _11_15_wpf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public DataSet DataSet { get; set; }   
+        public SqlDataAdapter SqlDataAdapter { get; set; }
+        public SqlDataAdapter SqlBooksDataAdapter { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();                      
+
+            DataSet = new DataSet();
+
+            SqlConnection connection = null;
+            string connectionString = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
+
+            try
+            {                
+                connection = new SqlConnection(connectionString);
+                SqlDataAdapter = GetAuthorsAdapter(connection);
+                UpdateGridDataSource();
+
+
+                SqlBooksDataAdapter = GetBooksAdapter(connection);
+                UpdateGridBooksDataSource();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+           
+
+            buttonFill.Click += ButtonFill_Click;
+            buttonUpdate.Click += ButtonUpdate_Click;
+            buttonDelete.Click += ButtonDelete_Click;
+            btnFill.Click += BtnFill_Click;
+            btnUpdate.Click += BtnUpdate_Click;
+            btnDelete.Click += BtnDelete_Click;
+
+
+
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (grid2.SelectedItems != null)
+                {
+                    for (int i = 0; i < grid2.SelectedItems.Count; i++)
+                    {
+                        DataRow dataRow = (grid2.SelectedItems[i] as DataRowView).Row;
+                        if (dataRow != null)
+                        {
+                            dataRow.Delete();
+                        }
+                    }
+                }
+                SqlBooksDataAdapter.Update(DataSet, "Books");
+                UpdateGridBooksDataSource();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void BtnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SqlBooksDataAdapter.Update(DataSet, "Books");
+                UpdateGridBooksDataSource();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void BtnFill_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {                
+                UpdateGridBooksDataSource();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void ButtonFill_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {               
+                UpdateGridDataSource();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void ButtonDelete_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (grid.SelectedItems != null)
+                {
+                    for (int i = 0; i < grid.SelectedItems.Count; i++)
+                    {
+                        DataRow dataRow = (grid.SelectedItems[i] as DataRowView).Row;
+                        if (dataRow != null)
+                        {
+                            dataRow.Delete();
+                        }
+                    }
+                }
+                SqlDataAdapter.Update(DataSet, "Authors");
+                UpdateGridDataSource();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void ButtonUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SqlDataAdapter.Update(DataSet, "Authors");
+                UpdateGridDataSource();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        ///
+
+        public void UpdateGridDataSource()
+        {
+            //  очищаем ДатаСет перед повторным заполнением
+            //DataSet.Clear();
+
+            if (DataSet.Tables.Contains("Authors"))
+                DataSet.Tables["Authors"].Clear();
+
+            //  заполняем ДатаСет новыми данными из таблицы Афторс
+            SqlDataAdapter.Fill(DataSet, "Authors");
+
+            //  Устанавливаем источние данных для грида
+            grid.ItemsSource = DataSet.Tables["Authors"].DefaultView;
+        }
+
+        public void UpdateGridBooksDataSource()
+        {
+           // DataSet.Clear();
+
+            if (DataSet.Tables.Contains("Books"))
+                DataSet.Tables["Books"].Clear();
+
+            SqlBooksDataAdapter.Fill(DataSet, "Books");
+            
+            grid2.ItemsSource = DataSet.Tables["Books"].DefaultView;
+        }
+
+        ///
+
+        public static SqlDataAdapter GetAuthorsAdapter(SqlConnection connection)
+        {
+            string selectCommand = @"SELECT * FROM Authors";
+
+            SqlDataAdapter authorsAdapter = new SqlDataAdapter(selectCommand, connection);
+            SqlCommandBuilder builder = new SqlCommandBuilder(authorsAdapter);
+
+            return authorsAdapter;
+        }
+
+        public static SqlDataAdapter GetBooksAdapter(SqlConnection connection)
+        {
+            string selectCommand = @"SELECT * FROM Books";
+
+            SqlDataAdapter booksAdapter = new SqlDataAdapter(selectCommand, connection);
+            SqlCommandBuilder builder = new SqlCommandBuilder(booksAdapter);
+
+            return booksAdapter;
+        }
+
+
+
+    }
+}
---
+++ C#/11-18/11-18_LINQ/Program.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_18_LINQ
+{
+
+    internal class Program
+    {
+        static readonly string[] countries = {"Albania", "UK",
+        "Lithuania", "Andorra", "Austria",
+        "Latvia", "Liechtenstein", "Switzerland",
+        "Ireland", "Sweden","Italy", "France",
+        "Liechtenstein", "Spain", "Turkey", "Germany",
+        "Switzerland", "Monaco", "Montenegro",
+        "Norway", "Finland", "Turkey", "UK", "Poland",
+        "Portugal", "Lithuania", "Luxembourg"
+    };
+
+        static void Main(string[] args)
+        {
+
+        // LINQ query
+        //var result = countries.Where(c => c.StartsWith("L")).Select(c => c);
+
+        //Console.WriteLine("Countries begining with L:");
+
+        //foreach (var item in countries) 
+        //        Console.WriteLine(item);
+                
+        //Console.WriteLine();
+
+
+        //var countryMoreFive = countries.Where(c => c.Length > 5);
+        //    foreach (var item in countries)
+        //        Console.WriteLine(item);
+
+        
+            var result2 = from c in countries 
+                      where c.StartsWith("L") 
+                      select c;
+
+            ///
+            var a = new[] { 0, 1, 2 };
+            /// anon is complited as an anonymus type
+            var anon = new { Name = "Terry", Age = 34 };
+            ///
+            var list = new List<int>();
+
+
+
+        var products = GenerateProductList();
+
+            //Console.WriteLine(products.Count());   
+
+            //foreach (var p in products)
+            //    Console.WriteLine(p);
+
+            //Console.WriteLine();
+
+
+            //var productNameLess500 = products.Where(p => p.Price < 500).Select(p => p.Name.ToUpper());
+            //foreach (var p in productNameLess500)
+            //    Console.WriteLine(p);
+
+            //Console.WriteLine();
+
+
+            //var productNameLess500 =from p in products
+            //                        where p.Price < 500
+            //                        orderby p.Price
+            //                        select p.Name.ToUpper();
+            //foreach (var p in productNameLess500)
+            //    Console.WriteLine(p);
+
+
+            //var productNameMore8 = from p in products
+            //                         where p.Count > 8                                     
+            //                         select p.Name.ToLower();
+            //foreach (var p in productNameMore8)
+            //    Console.WriteLine(p);
+
+
+            //var productNameAndPriceLess500 = products.Where(p =>  p.Price <500).
+            //                                    Select(p => new { Product = p.Name, 
+            //                                                      Price = p.Price });
+            //foreach (var item in productNameAndPriceLess500)
+            //        Console.WriteLine(item);
+
+            //var productNameAndPriceLess500 = from p in products
+            //                                 where p.Price < 500
+            //                                 select new
+            //                                 {
+            //                                     Product = p.Name,
+            //                                     Price = p.Price
+            //                                 };
+            //foreach (var item in productNameAndPriceLess500)
+            //        Console.WriteLine(item);
+
+
+
+            //var productCountyL = from p in products
+            //                    where p.Manufacturer.StartsWith("L")
+            //                    orderby p.Price  descending   // revers sorting
+            //                    select p;
+            //foreach (var c in productCountyL)
+            //    Console.WriteLine(c);
+
+
+            //var productsOrderByCount = from p in products
+            //                           orderby p.Count descending
+            //                           select p;
+
+            //foreach (var p in productsOrderByCount.Take(2))
+            //    Console.WriteLine(p);
+            //Console.WriteLine();
+            //foreach (var p in productsOrderByCount.Skip(45))
+            //    Console.WriteLine(p);
+
+
+            
+            var productManufacturerMore6 = from p in products
+                                           where p.Manufacturer.Length > 6
+                                           orderby p.Count
+                                           select p;
+            foreach (var p in productManufacturerMore6.Take(5))
+                Console.WriteLine(p);
+
+            Console.WriteLine();
+
+
+            var last3Product = from p in products
+                               orderby p.Price
+                               select new { p.Manufacturer, p.Price };
+            foreach (var product in last3Product.Take(5))
+               Console.WriteLine(product);
+
+
+
+
+
+            var prodFromItaly = (from p in products
+                                 where p.Manufacturer == "Italy"
+                                 select p.Price).Average();
+            //      =
+            var prodFromItaly2 = products.Where(p => p.Manufacturer == "Italy").
+                                            Select(p => p.Price).
+                                            Average();
+
+
+
+            var sumProdContWhereManufacturerT = products.Where(p => p.Manufacturer.StartsWith ("T")).
+                                                         Select(p => p.Count).
+                                                         Sum();
+            //      =
+            var sumProdContWhereManufacturerT2 = (from p in products
+                                              where p.Manufacturer.StartsWith("T")
+                                              select p.Count).Sum();
+
+
+        }
+
+        static IEnumerable <Product> GenerateProductList()
+        {
+            var rand = new Random();
+            List<Product> products = new List<Product>();
+            for (int i = 0; i < 100; i++)
+            {
+                products.Add(new Product
+                {
+                    Name = "Product" + (++i),
+                    Price = rand.Next(0, 1000),
+                    Manufacturer = countries[rand.Next(0, countries.Length - 1)],
+                    Count = rand.Next(0, 10)
+                });
+            }
+        return products;
+        }
+    }
+
+
+    class Product 
+    { 
+        public string Name { get; set; }
+        public int  Price { get; set; }
+        public string Manufacturer { get; set; }
+        public int Count { get; set; }
+        public override string ToString()
+        {
+            return $"{Name} {Price} {Manufacturer} {Count}";
+        }
+
+
+    }
+
+}
