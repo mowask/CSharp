5d4776a11a8d6d9587f258cfbd3aa9e28f2c9454
mowask <164919999+mowask@users.noreply.github.com>
2024-11-29 21:10:53 +0300

    Add files via upload

---
+++ C#/11-25 SQLite/Contexts/ExpensesTrackerContext.cs
@@ -0,0 +1,40 @@
+﻿using ExpensesTraker_Library.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpensesTraker_Library.Contexts
+{
+    public class ExpensesTrackerContext    : DbContext
+    {
+
+        // dbset для категорий расходов
+        public DbSet <Category> Categories { get; set; }
+
+        // dbset для самих расходов
+        public DbSet<Expense> Expenses { get; set; }
+
+        //конструктор класса
+        public ExpensesTrackerContext()
+        {
+            // удаляем существующую базу данных, если она есть
+            //  Database.EnsureDeleted();
+
+            // создаем новую базу данных, если ее нет
+            Database.EnsureCreated();
+        }
+
+
+        // метод для настройки подключения к базе данных
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // используем SQLite базу данных с указанным именем
+            optionsBuilder.UseSqlite("Data Source=Expenses.db");
+        }
+
+
+    }
+}
---
+++ C#/11-25 SQLite/Repositories/ExpenseRepository.cs
@@ -0,0 +1,108 @@
+﻿using ExpensesTraker_Library.Contexts;
+using ExpensesTraker_Library.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpensesTraker_Library.Repositories
+{
+    public class ExpenseRepository
+    {
+        private readonly ExpensesTrackerContext _context;
+
+        public ExpenseRepository(ExpensesTrackerContext context)
+        {
+            _context = context;
+        }
+
+
+        public ObservableCollection<Category> GetAllCategoriesObservable()
+        {
+            _context.Categories.Load();
+            return _context.Categories.Local.ToObservableCollection();
+        }
+
+
+        public void AddCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges(true);
+        }
+
+
+        public void RemoveCategory(int categoryId)
+        {
+            var category = _context.Categories.Find(categoryId);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+                _context.SaveChanges(true);
+            }
+        }
+
+
+        public IEnumerable<Category> GetAllCategories()
+        {
+            return _context.Categories.ToList();
+        }
+
+
+        public void AddExpense(Expense expense)
+        {
+            _context.Expenses.Add(expense);
+            _context.SaveChanges(true); 
+        }
+
+
+
+        public void RemoveExpense(int expenseId)
+        {
+            var expense = _context.Expenses.Find(expenseId);
+            if (expense != null)
+            {
+                _context.Expenses.Remove(expense);
+                _context.SaveChanges(true);
+            }
+        }       
+
+
+         public IEnumerable<Expense> GetAllExpenses () 
+         {
+            return _context.Expenses.ToList();
+         }
+
+
+        public ObservableCollection<Expense> GetAllExpensesObservable()
+        {
+            _context.Expenses.Include(e => e.Category).Load();
+            return _context.Expenses.Local.ToObservableCollection();
+        }
+
+
+        public void DeleteCategory(int categoryId)
+        {
+            var category = _context.Categories.Find(categoryId);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+                _context.SaveChanges(true);
+            }
+        }
+
+
+        public void DeleteCategories (List<Category> category)
+        {
+            _context.Categories.RemoveRange(category);
+            _context.SaveChanges(true);
+        }
+
+
+      
+
+
+    }
+}
